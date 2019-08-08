3e01c8b59e61d13f6f71008e5108395de18daa2e
Alexander <Alexander-e-mail@yandex.ru>
2019-08-08 13:06:30 +0300

    GetBlackImage(string path) add, - but just save BlackImage on server

--- MyWebAPI/Controllers/ValuesController.cs
+++ MyWebAPI/Controllers/ValuesController.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using System.Drawing;
 
 namespace MyWebAPI.Controllers
 {
@@ -22,6 +25,41 @@ namespace MyWebAPI.Controllers
         public ActionResult<string> Get(int id)
         {
             return "value";
+        }
+
+        [HttpGet("image")]
+        public ActionResult<string> GetBlackImage(string path)
+        {
+            Stream rtn = null;
+            HttpWebRequest aRequest = (HttpWebRequest)WebRequest.Create(path);
+            HttpWebResponse aResponse = (HttpWebResponse)aRequest.GetResponse();
+
+            rtn = aResponse.GetResponseStream();
+
+            Image img = Image.FromStream(rtn);
+            Bitmap newIm = new Bitmap(img);
+
+            for (int i = 0; i < newIm.Width; i++)
+            {
+                for (int j = 0; j < newIm.Height; j++)
+                {
+                    UInt32 pixel = (UInt32)(newIm.GetPixel(i, j).ToArgb());
+                    // получаем компоненты цветов пикселя
+                    float R = (float)((pixel & 0x00FF0000) >> 16); // красный
+                    float G = (float)((pixel & 0x0000FF00) >> 8); // зеленый
+                    float B = (float)(pixel & 0x000000FF); // синий
+                                                           // делаем цвет черно-белым (оттенки серого) - находим среднее арифметическое
+                    R = G = B = (R + G + B) / 3.0f;
+                    // собираем новый пиксель по частям (по каналам)
+                    UInt32 newPixel = 0xFF000000 | ((UInt32)R << 16) | ((UInt32)G << 8) | ((UInt32)B);
+                    // добавляем его в Bitmap нового изображения
+                    newIm.SetPixel(i, j, Color.FromArgb((int)newPixel));
+                }
+            }
+            newIm.Save("Black.jpg");
+            
+
+            return img.Width.ToString(); 
         }
 
         // POST api/values
