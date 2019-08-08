8d4588f59eb3274777828ee49a4a202c5d4ffa92
Alexander <Alexander-e-mail@yandex.ru>
2019-08-08 13:40:22 +0300

    BlackAndWhite file Maked, saved on Server as Black.jpg and return Bitmap from Get(string url)

--- MyWebAPI/Controllers/ValuesController.cs
+++ MyWebAPI/Controllers/ValuesController.cs
@@ -6,6 +6,7 @@ using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using System.Drawing;
+using MyWebAPI.Utilities;
 
 namespace MyWebAPI.Controllers
 {
@@ -28,38 +29,10 @@ namespace MyWebAPI.Controllers
         }
 
         [HttpGet("image")]
-        public ActionResult<string> GetBlackImage(string path)
+        public ActionResult<Bitmap> GetBlackImage(string path)
         {
-            Stream rtn = null;
-            HttpWebRequest aRequest = (HttpWebRequest)WebRequest.Create(path);
-            HttpWebResponse aResponse = (HttpWebResponse)aRequest.GetResponse();
-
-            rtn = aResponse.GetResponseStream();
-
-            Image img = Image.FromStream(rtn);
-            Bitmap newIm = new Bitmap(img);
-
-            for (int i = 0; i < newIm.Width; i++)
-            {
-                for (int j = 0; j < newIm.Height; j++)
-                {
-                    UInt32 pixel = (UInt32)(newIm.GetPixel(i, j).ToArgb());
-                    // получаем компоненты цветов пикселя
-                    float R = (float)((pixel & 0x00FF0000) >> 16); // красный
-                    float G = (float)((pixel & 0x0000FF00) >> 8); // зеленый
-                    float B = (float)(pixel & 0x000000FF); // синий
-                                                           // делаем цвет черно-белым (оттенки серого) - находим среднее арифметическое
-                    R = G = B = (R + G + B) / 3.0f;
-                    // собираем новый пиксель по частям (по каналам)
-                    UInt32 newPixel = 0xFF000000 | ((UInt32)R << 16) | ((UInt32)G << 8) | ((UInt32)B);
-                    // добавляем его в Bitmap нового изображения
-                    newIm.SetPixel(i, j, Color.FromArgb((int)newPixel));
-                }
-            }
-            newIm.Save("Black.jpg");
-            
-
-            return img.Width.ToString(); 
+            BlackAndWhite baw = new BlackAndWhite(path);
+            return baw.BlackAndWhiteBrush(); 
         }
 
         // POST api/values
---
+++ MyWebAPI/Utilities/BlackAndWhite.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace MyWebAPI.Utilities
+{
+    public class BlackAndWhite
+    {
+        public string path;
+        public BlackAndWhite(string _path)
+        {
+            path = _path;
+        }
+
+        public Bitmap BlackAndWhiteBrush()
+        {
+            Stream rtn = null;
+            HttpWebRequest aRequest = (HttpWebRequest)WebRequest.Create(path);
+            HttpWebResponse aResponse = (HttpWebResponse)aRequest.GetResponse();
+
+            rtn = aResponse.GetResponseStream();
+
+            Image img = Image.FromStream(rtn);
+            Bitmap newIm = new Bitmap(img);
+
+            for (int i = 0; i < newIm.Width; i++)
+            {
+                for (int j = 0; j < newIm.Height; j++)
+                {
+                    UInt32 pixel = (UInt32)(newIm.GetPixel(i, j).ToArgb());
+                    float R = (float)((pixel & 0x00FF0000) >> 16); 
+                    float G = (float)((pixel & 0x0000FF00) >> 8); 
+                    float B = (float)(pixel & 0x000000FF); 
+                    R = G = B = (R + G + B) / 3.0f;
+                    UInt32 newPixel = 0xFF000000 | ((UInt32)R << 16) | ((UInt32)G << 8) | ((UInt32)B);
+                    newIm.SetPixel(i, j, Color.FromArgb((int)newPixel));
+                }
+            }
+            newIm.Save("Black.jpg");
+
+            return newIm;
+        }
+    }
+}
